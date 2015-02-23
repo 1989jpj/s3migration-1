78af690e6e0045eef018e03e530bcd0138a00ab1
sbenner <sergey.benner@gmail.com>
2015-02-23 13:12:01 +0300

    cleanup

--- src/S3Utility.java
+++ src/S3Utility.java
@@ -6,7 +6,6 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.*;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
@@ -19,23 +18,21 @@ public class S3Utility {
     static AmazonS3 amazonS3;
     static Properties properties = new Properties();
 
-
-
-    private static void loadProperties() throws FileNotFoundException,IOException {
+    private static void loadProperties() throws IOException {
 
         InputStream is = new FileInputStream("s3migration.properties");
         properties.load(is);
 
     }
-    private static String getValueFromProperties(String keyName){
-       return properties.getProperty(keyName);
-    }
 
+    private static String getValueFromProperties(String keyName) {
+        return properties.getProperty(keyName);
+    }
 
     private static void init() throws Exception {
 
         loadProperties();
-        dreamObjectsS3 = new AmazonS3Client(new BasicAWSCredentials(getValueFromProperties("doKey"),getValueFromProperties("doSecret") ));
+        dreamObjectsS3 = new AmazonS3Client(new BasicAWSCredentials(getValueFromProperties("doKey"), getValueFromProperties("doSecret")));
         dreamObjectsS3.setEndpoint("objects.dreamhost.com");
 
         amazonS3 = new AmazonS3Client(new BasicAWSCredentials(getValueFromProperties("awsKey"), getValueFromProperties("awsSecret")));
@@ -71,7 +68,7 @@ public class S3Utility {
 
                         if (!objectSummary.getKey().endsWith(".mov") && dateAfter != null && objectSummary.getLastModified().getTime() >= dateAfter)
                             uploadObject(amazonS3, downloadObject(dreamObjectsS3, sourceBucket, objectSummary.getKey()), destBucket);
-                        else if (!objectSummary.getKey().endsWith(".mov")&&dateAfter == null)
+                        else if (!objectSummary.getKey().endsWith(".mov") && dateAfter == null)
                             uploadObject(amazonS3, downloadObject(dreamObjectsS3, sourceBucket, objectSummary.getKey()), destBucket);
 
                     } catch (Exception e) {
@@ -91,17 +88,9 @@ public class S3Utility {
             System.out.println("You have " + buckets.size() + " Amazon S3 bucket(s), " +
                     "containing " + totalItems + " objects with a total size of " + totalSize + " bytes.");
         } catch (AmazonServiceException ase) {
-            /*
-             * AmazonServiceExceptions represent an error response from an AWS
-             * services, i.e. your request made it to AWS, but the AWS service
-             * either found it invalid or encountered an error trying to execute
-             * it.
-             */
-
+            ase.printStackTrace();
         } catch (AmazonClientException ace) {
             ace.printStackTrace();
-
-            System.out.println("Error Message: " + ace.getMessage());
         }
     }
 
@@ -154,8 +143,7 @@ public class S3Utility {
         String sourceBucket = args[0];
         String destBucket = args[1];
 
-        if (args.length==3&&args[2] != null && !args[2].isEmpty())
-        {
+        if (args.length == 3 && args[2] != null && !args[2].isEmpty()) {
             try {
                 dateAfter = Long.parseLong(args[2]);
             } catch (NumberFormatException nfe) {
@@ -165,10 +153,9 @@ public class S3Utility {
         }
         init();
         System.out.println("=================dreamObjectsS3==========================");
-        // downloadObject(dreamObjectsS3,"stagebucket","2014-11-25-15/3edc12ac-a24c-450e-b5b1-4665404db716-thumb.jpg");
         listObjectsFromS3(dreamObjectsS3, sourceBucket, destBucket, dateAfter);
         System.out.println("=================amazonS3==========================");
-        //listObjectsFromS3(amazonS3);
+
 
     }
 }
